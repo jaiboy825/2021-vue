5023e90e83bd979c061f21cf1ae4a0be49eb3a98
jaiboy825 <jaiboy@naver.com>
2021-04-01 18:39:06 +0900

    0401 업로드

---
+++ vue_project/Chap01_Component/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>Vue Component</h1>
+    <Currency/>
+  </div>  
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+  import Currency from './components/A01.Currency'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    Currency
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
